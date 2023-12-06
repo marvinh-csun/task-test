156396c5c18c723f8a2ed4971d19c1d7b0583a85
marvinh-csun <marvin.harootoonyan@csun.edu>
2023-12-05 20:36:01 -0800

    init commit

---
+++ src/test/java/com/comp584/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp584.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
